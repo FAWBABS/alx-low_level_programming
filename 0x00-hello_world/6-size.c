6ea6c74ac156f39d1c335f564bd122e23996412e
Fawaz <fawazefbee@gmail.com>
2022-06-30 15:00:47 -0700

    first

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,7 +12,7 @@ int main(void)
 	long int b;
 	long long int c;
 	char d;
-	float;
+	float f;
 
 	printf("size of char: %lu byte(s)\n", (unsigned long)sizeof(d));
 	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(a));
