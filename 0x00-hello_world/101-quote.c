068c83da182dbdb2b19b152549b2f176fef390aa
Fawaz <fawazefbee@gmail.com>
2022-07-01 04:33:04 -0700

    first

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,13 @@
-#include<stdio.h>
+#include <unistd.h>
+#include <sys/syscall.h>
 /**
  *main - Entry point
  *
- * Return: 1(Success)
+ * Return: Always 1(Success)
  */
 int main(void)
 {
-	putchar(""and that piece of art is useful" - Dora Korpar, 2015-10-19");
+	const char quest [] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	syscall(SYS_write, 1, quest, sizeof(quest))
 	return (1);
 }
