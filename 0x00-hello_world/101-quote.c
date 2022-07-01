88c4131d734e9e9d2c4d7007a5d39e06d1486489
Fawaz <fawazefbee@gmail.com>
2022-07-01 04:36:54 -0700

    first

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,11 +3,11 @@
 /**
  *main - Entry point
  *
- * Return: Always 1(Success)
+ * Return: Always 1 (Success)
  */
 int main(void)
 {
-	const char quest [] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	syscall(SYS_write, 1, quest, sizeof(quest))
+	const char quest[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	syscall(SYS_write, 1, quest, sizeof(quest));
 	return (1);
 }
