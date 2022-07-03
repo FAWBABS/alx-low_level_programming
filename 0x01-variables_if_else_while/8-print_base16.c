0afcd3882810ba23365abff0f51bd0b8fd6797a3
Fawaz <fawazefbee@gmail.com>
2022-07-03 10:05:55 -0700

    go

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - printing numbers in base 16 in lowercase
+ *
+ * Return: Always 0(Success)
+ */
+int main(void)
+{
+	int ch;
+
+	for (ch = 0; ch <= 10; ch++)
+	{
+		putchar(ch);
+	}
+	for (ch = 'a'; ch <= 'e'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
