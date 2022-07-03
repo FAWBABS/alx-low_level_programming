758d7caf2431a549a7bbcd470c39c67358b66c71
Fawaz <fawazefbee@gmail.com>
2022-07-03 06:34:55 -0700

    go

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - print alphaets in reverse z-a
+ *
+ * Return: Always 0(Success);
+ */
+int main(void)
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar("%c", ch);
+	}
+	putchar('\n');
+
+	return (0);
+}
