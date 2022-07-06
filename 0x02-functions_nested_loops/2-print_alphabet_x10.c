c7543ea52bcbf9f88d5697241f373c4dacf241ea
Fawaz <fawazefbee@gmail.com>
2022-07-06 08:10:40 -0700

    f

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Print alpahbets in lowercase x10 times
+ *
+ * Return: Always 0(Success)
+ */
+void print_alphabet_x10(void)
+{
+	int ch;
+	int count;
+
+	count = 0
+	while (count < 10)
+	{
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		_putchar(ch)
+	}
+
+	count++;
+	_putchar('\n');
+	}
+
+}
