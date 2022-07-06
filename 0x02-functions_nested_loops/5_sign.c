bc0af610ff7c39758a9c9c93255e6a605f483fe4
Fawaz <fawazefbee@gmail.com>
2022-07-06 14:52:47 -0700

    f

---
+++ 0x02-functions_nested_loops/5_sign.c
@@ -0,0 +1,27 @@
+#include "main"
+/**
+ * print_sign - determines sign of a number
+ *
+ * @n: number to be considered
+ *
+ * Return: Alwats 0
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	if (n == 0)
+	{
+		_putchar(0);
+		return (0);
+	}
+	if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+
+}
