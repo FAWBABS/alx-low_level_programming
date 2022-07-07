2fbf7cd719a80f9f1411285f8759ec3e175aa51e
Fawaz <fawazefbee@gmail.com>
2022-07-07 07:08:28 -0700

    a

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -16,7 +16,7 @@ if ((year % 400 == 0 || (year % 4 == 0 && year % 100 != 0))
 if (month >= 3 && day >= 60)
 {
 day++;
-}
+
 
 
 printf("Day of the year: %d\n", day);
@@ -32,6 +32,5 @@ else
 {
 printf("Day of the year: %d\n", day);
 printf("Remaining days: %d\n", 365 - day);
-}
 }
 }
