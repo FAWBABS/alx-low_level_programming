60c2475597fdfcf9c73dbe7d3e8cfc4104cbf707
Fawaz <fawazefbee@gmail.com>
2022-07-08 04:14:16 -0700

    f

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -7,3 +7,6 @@ int postitive_or_negative(void)
 int largest_number(int a, int b, int c)
 void print_remaining_days(int month, int day, int year)
 int convert_day(int month, int day)
+void print_times_table(int n);
+
+#endif
