9650a211847a9511dbfa1ebd04fbca354ffc3dec
Fawaz <fawazefbee@gmail.com>
2022-07-08 09:22:23 -0700

    a

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -3,10 +3,12 @@
 
 #include <stdio.h>
 
-int postitive_or_negative(void)
-int largest_number(int a, int b, int c)
-void print_remaining_days(int month, int day, int year)
-int convert_day(int month, int day)
-void print_times_table(int n);
+int postitive_or_negative(void);
+int largest_number(int a, int b, int c);
+void print_remaining_days(int month, int day, int year);
+int convert_day(int month, int day);
+int some_functions(int i);
+
+
 
 #endif /* MAIN_H */
