db313804b5ab16aecdff4dc7d0e8ed5000fff09f
Fawaz <fawazefbee@gmail.com>
2022-07-13 15:29:07 -0700

    l

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#inlude "main.h"
+
+/**
+ * swap_int - swap the values of two integers
+ * @a: A pointer to an int
+ * @b: B pointer to an int
+ * Return: vpoid means answer is correct
+ */
+
+void swap_int(int *a, int *b)
+{
+	int f;
+
+	f = *a;
+	*a = *b;
+	*b = f;
+}
