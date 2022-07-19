fe4907b0875f71ed108118fc0612fb44265d0cab
Fawaz <fawazefbee@gmail.com>
2022-07-19 09:44:48 -0700

    pointers

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -15,7 +15,7 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 	char *char_src = (char *) src;
 
 	for (int i = 0; i < n; i++)
-		dest[i] = src[i];
+		char_dest[i] = char_src[i];
 
 	return (dest);
 }
