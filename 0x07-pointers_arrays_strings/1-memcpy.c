51a2854efdbfb822062815bea25bc86c7047f39d
Fawaz <fawazefbee@gmail.com>
2022-07-19 10:11:24 -0700

    pointers

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -12,9 +12,10 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 	unsigned int i;
 	i = 0;
 
-	for (int i = 0; i < n; i++)
+	while (i < n)
 	{
 		*(dest + i) = *(src + i);
+		i++;
 	}
 
 	return (dest);
