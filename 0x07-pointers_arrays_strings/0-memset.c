79a49b22666e9a926162feceb9fdfa7d9ed06b5d
Fawaz <fawazefbee@gmail.com>
2022-07-19 08:08:26 -0700

    pointers

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -4,8 +4,15 @@
  * @s: memory pointed
  * @b: constant byte
  * @n: integer to be considered
+ * Return: Always 0
  */
 char *_memset(char *s, char b, unsigned int n)
 {
+	unsigned int i;
 
+	for (i = 0; i < n; i++)
+	{
+		*(s + i) = b;
+	}
+	return (s);
 }
