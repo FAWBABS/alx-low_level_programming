0ef3294209387ffd0477ba47f48b94a17f589216
Fawaz <fawazefbee@gmail.com>
2022-07-19 14:21:11 -0700

    pointers

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -12,14 +12,11 @@ void print_chessboard(char (*a)[8])
 	while (i < 8)
 	{
 		j = 0;
-		while (i < 0)
+		while (j < 8)
 		{
-			j = 0;
-			while (j < 8)
-			{
-				_putchar (a[i][j]);
-				j++;
-			}
+			_putchar (a[i][j]);
+			j++;
+		}
 			_putchar ('\n');
 			i++;
 		}
