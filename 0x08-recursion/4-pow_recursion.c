b93492fed7dec3a6c13fe3a13970bbc2e62abc30
Fawaz <fawazefbee@gmail.com>
2022-07-21 07:58:34 -0700

    recursion

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -15,9 +15,8 @@ int _pow_recursion(int x, int y)
 	{
 		return (1);
 	}
-	else if (y == 1)
+	else if (y != 0)
 	{
-		return (x);
+		return (_pow_recursion((x, y - 1) * x));
 	}
-	return (_pow_recursion((x, y - 1) * x));
 }
