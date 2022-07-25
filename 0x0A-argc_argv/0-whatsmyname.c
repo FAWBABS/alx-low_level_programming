36e3f5951e117dd5d79742826546f6684d223e08
Fawaz <fawazefbee@gmail.com>
2022-07-25 07:40:33 -0700

    arguments

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
+#include "main.h"
 /**
  * main - prints its name
  * @argc: number of items in command line
  * @argv: array size of the command line
  * Return: void
  */
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
 	printf("%s\n", argv[0])
 		return (0);
