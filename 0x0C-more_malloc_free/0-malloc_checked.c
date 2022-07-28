43f26205cf1138cb1c9c38783868b4415ade53f9
Fawaz <fawazefbee@gmail.com>
2022-07-28 10:25:33 -0700

    free

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * mallloc_checked - allocates memory
+ * @b: int
+ * Return: void
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *p;
+
+	p = malloc(b);
+	if (p == NULL)
+	{
+		exit(98);
+	}
+	return (p);
+}
