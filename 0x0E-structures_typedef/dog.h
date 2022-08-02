6841bcb9d649b5b366811a0686b8b21069d016c3
Fawaz <fawazefbee@gmail.com>
2022-08-02 12:59:20 -0700

    struct

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,15 @@
 #ifndef STRUCT_DOG
 #define STRUCT_DOG
 
+/**
+ * struct dog - Struct dog
+ * @name: name
+ * @age: age
+ * @owner: owner
+ *
+ * Description: This struct is for dog
+ */
+
 struct dog
 {
 	char *name;
