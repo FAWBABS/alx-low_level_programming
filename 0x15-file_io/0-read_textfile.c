44056125c95a3f571a8320de9391b09bc4fa2d5f
Fawaz <fawazefbee@gmail.com>
2022-08-23 11:30:54 -0700

    file

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -15,12 +15,13 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (!filename)
 		return (0);
 
-	fd = open(filename, 0_RDONLY);
+
+	fd = open(filename, O_RDONLY);
 
 	if (fd == -1)
 		return (0);
 
-	bf = malloc(sizeof(char) * (letters));
+	buf = malloc(sizeof(char) * (letters));
 	if (!buf)
 		return (0);
 
