1060f5eae9e83077bb74e8a90467768c4fbb1ccc
Fawaz Babaodutayo <fawazbabaodutayo@gmail.com>
2022-08-24 15:03:23 +0100

    file

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,11 +1,13 @@
 #include "main.h"
 
 /**
- * read_textfile -  reads a text file and prints it to the POSIX.
- * @filename: pointer to text in a file
- * @letters: number of letters
- * Return: the actual number of letters it could read and print
+ * read_textfile - Reads a text file and prints the letters
+ * @filename: Filename
+ * @letters: Numbers of letters printed
+ * Return: Numbers of letters printed on success
+ * or 0 otherwise
  */
+
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
@@ -14,7 +16,6 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 	if (!filename)
 		return (0);
-
 
 	fd = open(filename, O_RDONLY);
 
@@ -32,5 +33,5 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 	free(buf);
 
-	return (0);
+	return (nwr);
 }
