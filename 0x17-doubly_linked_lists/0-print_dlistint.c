95cf43dc727bd29df4c30acf2ea698bb88088ac9
FAWBABS <fawazbabaodutayo@gmail.com>
2022-10-03 14:51:00 +0000

    task 1

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -6,7 +6,7 @@
  *
  * Return: The number of modes int the list
  */
-size_t print_dlistint(const dlistint_t *h);
+size_t print_dlistint(const dlistint_t *h)
 {
 	size_t nodes = 0;
 
@@ -14,8 +14,8 @@ size_t print_dlistint(const dlistint_t *h);
 	{
 		nodes++;
 		printf("%d\n", h->n);
-		h = h->next
+		h = h->next;
 	}
 
-	return (nodes)
+	return (nodes);
 }
